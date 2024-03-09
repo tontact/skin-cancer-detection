f819a8127eeb0f905e476463ccd8a8fcc042b61f
tontact <45295230+tontact@users.noreply.github.com>
2024-03-10 04:03:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def prediction(image_array):
     return pred
 
 def main():
-    st.title(<h1 style='color: blue;'>Prediction App</h1>)
+    st.title("<h1 style='color: blue;'>Prediction App</h1>")
     st.header('Skin Cancer Classification')
     st.text("Upload a skin Image for image classification")
 
